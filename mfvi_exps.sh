fa9923e45a8513a577a909b17c4b01325ffa29c9
maltetoelle <>
2021-02-14 12:46:01 +0100

    bash script

---
+++ mfvi_exps.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python python denoising.py --net_specs '{"prior_mu": 0., "prior_sigma": 0.05, "kl_type": "forward", "beta": 1e-6}' --optim_specs '{"lr": 0.01}' --num_iter 100000 --img_name oct --exp_name mfvi_oct_100k --gpu $1 --log_dir $2
+
+python python denoising.py --net_specs '{"dropout_type": "2d", "dropout_p": 0.3}' --optim_specs '{"lr": 3e-4. "weight_decay": 1e-4}' --seed 42 --num_iter 100000 --img_name oct --exp_name mcd_oct_100k --gpu $1 --log_dir $2
+
+python python denoising.py --net_specs '{"dropout_type": "2d", "dropout_p": 0.3}' --optim_specs '{"lr": 3e-4. "weight_decay": 1e-4}' --seed 42 --num_iter 100000 --img_name us --exp_name mcd_us_100k --gpu $1 --log_dir $2
